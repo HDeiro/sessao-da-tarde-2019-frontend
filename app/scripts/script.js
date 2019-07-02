0b227318ec3284fe9033984cf5090b2044213bf7
Hugo <hugodeiro@gmail.com>
2019-07-02 08:15:19 -0300

    Dynamic menu loading

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,8 +1,24 @@
-$(document).ready(function () {
+const $ = document.querySelector.bind(document);
 
-    $('#sidebarCollapse').on('click', function () {
-		console.log('A')
-        $('#sidebar').toggleClass('active');
-    });
+window.addEventListener('load',  event => {
+	// Controles do menu
+	const menuNavigation = $('.menu-navigation-items');
 
+	let menuItems = [
+		{label: "Lista de Livros", link: "lista-livros.html"},
+		{label: "Registro de Livros", link: "registro-livros.html"},
+	];
+
+	menuItems.forEach(menu => {
+		let menuNavigationItem = document.createElement('li');
+		menuNavigationItem.classList.add('menu-navigation-item');
+
+		let menuNavigationItemLink = document.createElement('a');
+		menuNavigationItemLink.classList.add('menu-navigation-item-link');
+		menuNavigationItemLink.href = menu.link;
+		menuNavigationItemLink.innerText = menu.label;
+
+		menuNavigationItem.appendChild(menuNavigationItemLink);
+		menuNavigation.appendChild(menuNavigationItem);
+	});
 });
