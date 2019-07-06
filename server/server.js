9a43f2e500ad31929219fe7d768c7f3db5619536
Hugo <hugodeiro@gmail.com>
2019-07-06 07:51:29 -0300

    Getting book list from server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const dataPayload = require('./data.json');
 
+app.use(cors());
 app.use(bodyParser.json());
+app.options('*', cors());
 
-app.get('/livros', (request, response) => response.send(dataPayload));
+app.get('/livros', (request, response) => response.send(JSON.stringify(dataPayload)));
 
 app.post('/livros', (request, response) => {
 	response.send('OK');
